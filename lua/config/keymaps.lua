e8615805a40d75b49dadd8f269fdecff49577475
byronlove111 <malik.chan94420@gmail.com>
2026-03-30 13:40:43 +0200

    disable n in normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -70,6 +70,9 @@ vim.cmd("cabbrev Q q")
 -- Effacer la recherche
 map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
 
+-- Désactiver n (next search result)
+map("n", "n", "<Nop>", { desc = "Disabled" })
+
 -- Supermaven toggle
 map("n", "<leader>us", "<cmd>SupermavenToggle<cr>", { desc = "Toggle Supermaven" })
 
